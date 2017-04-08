676b7dd1b238905ede1daccc5d06152096a8698b
Tip0k <a.tymofiev@gmail.com>
2017-04-08 15:25:55 +0300

    c6 done

--- lab2/src/lab2/AgeLimits.java
+++ lab2/src/lab2/AgeLimits.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2;
 
 /**
@@ -15,7 +10,9 @@ public class AgeLimits {
     private int max;
 
     public AgeLimits(int min, int max) {
-        if(min < 0 || min > max) throw new IllegalArgumentException();
+        if (min < 0 || min > max) {
+            throw new IllegalArgumentException();
+        }
         this.min = min;
         this.max = max;
     }
@@ -30,16 +27,31 @@ public class AgeLimits {
 
     @Override
     public String toString() {
-        if(min == max) return min + "+";
+        if (min == max) {
+            return min + "+";
+        }
         return min + "-" + max;
     }
-    
+
     public static String convertToString(AgeLimits ageLimits) {
         return ageLimits.getMin() + "-" + ageLimits.getMax();
     }
-    
+
     public static AgeLimits convertToAgeLimits(String ageLimitsStr) {
         String[] tmp = ageLimitsStr.split("-");
         return new AgeLimits(Integer.parseInt(tmp[0]), Integer.parseInt(tmp[1]));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+        AgeLimits x = (AgeLimits) obj;
+        if (x.min == this.min && x.max == this.max) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- lab2/src/lab2/Toy.java
+++ lab2/src/lab2/Toy.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2;
 
 /**
@@ -10,19 +5,20 @@ package lab2;
  * @author PEOPLE
  */
 public class Toy {
+
     private final String name;
     private final double price;
     private final AgeLimits ageLimits;
     private final String attribute;
     private final String pictureURL;
-    
-    public Toy (String name, double price, AgeLimits ageLimits, String attribute, String pictureURL) {
-        if(name.length() > 3) {
-            this.name = name; 
+
+    public Toy(String name, double price, AgeLimits ageLimits, String attribute, String pictureURL) {
+        if (name.length() > 3) {
+            this.name = name;
         } else {
             throw new IllegalArgumentException();
         }
-        if(price >= 0.0d) {
+        if (price >= 0.0d) {
             this.price = price;
         } else {
             throw new IllegalArgumentException();
@@ -31,41 +27,41 @@ public class Toy {
         this.attribute = attribute;
         this.pictureURL = pictureURL;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public double getPrice() {
         return price;
     }
-    
+
     public AgeLimits getAgeLimits() {
         return ageLimits;
     }
-    
+
     public String getAttribute() {
         return attribute;
     }
-    
+
     public String getPictureURL() {
         return pictureURL;
     }
-    
+
     @Override
     public String toString() {
         return name;
     }
-    
+
     public static Toy convertToToy(String someToyStr) {
         String[] tmp = someToyStr.split(",");
-        return new Toy(tmp[0], Double.parseDouble(tmp[1]), 
+        return new Toy(tmp[0], Double.parseDouble(tmp[1]),
                 AgeLimits.convertToAgeLimits(tmp[2]), tmp[3], tmp[4]);
     }
-    
+
     public static String convertToString(Toy someToy) {
-        return someToy.getName() + "," + someToy.getPrice() + "," + 
-                AgeLimits.convertToString(someToy.getAgeLimits()) + "," 
+        return someToy.getName() + "," + someToy.getPrice() + ","
+                + AgeLimits.convertToString(someToy.getAgeLimits()) + ","
                 + someToy.getAttribute() + "," + someToy.getPictureURL() + "\r\n";
-    }    
+    }
 }
